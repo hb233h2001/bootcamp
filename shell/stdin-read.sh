87a9b79fddf0404dd5f361c31acf2d9638bc569c
HamYoungJae <129055894+hb233h2001@users.noreply.github.com>
2024-06-21 19:58:55 +0900

    Upload stdin-read
    stdinを理解するためにreadコマンドを使用するコードを直接作成して実行してみました。

---
+++ shell/stdin-read.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ユーザーから名前入力を受ける
+echo "What is your name?"
+read name
+
+# ユーザーから年齢入力を受ける
+echo "What is your age?"
+read age
+
+# 入力された情報を出力する
+echo "Hello, $name! You are $age years old."
