891b1fbadbbf5eb0736b52357ada3e885057312f
HamYoungJae <129055894+hb233h2001@users.noreply.github.com>
2024-06-22 18:16:13 +0900

    Update judge-path.sh
    ホームディレクトリパスである~が絶対パスだと思っていましたが、相対パスであることに気づき、該当コードを削除しました。

--- shell/judge-path.sh
+++ shell/judge-path.sh
@@ -5,7 +5,7 @@ echo "ファイルパスを入力してください:"
 read FILE_PATH
 
 # 入力されたファイル経路が相対か絶対かを判別
-if [[ "$FILE_PATH" == /* || "$FILE_PATH" == ~* ]]; then  # TRYOUTはMac OS ユーザ想定なので、ウインドウのC:のような場合は排除しました。
+if [[ "$FILE_PATH" == /* ]]; then  # TRYOUTはMac OS ユーザ想定なので、ウインドウのC:のような場合は排除しました。
   echo "絶対パスです: $FILE_PATH"
 else
   echo "相対パスです: $FILE_PATH"
