69bd5b1b436ab0578e3e0b0c73194694c4c7b122
HamYoungJae <129055894+hb233h2001@users.noreply.github.com>
2024-06-24 15:07:07 +0900

    Update file-organization.sh

--- shell/original/file-organization.sh
+++ shell/original/file-organization.sh
@@ -2,14 +2,14 @@
 
 # 使い方（usage）を表示させる
 usage() {
-    echo "使い方: $0 <MMDD> [Directory to organize]"
-    echo "例: $0 0622 /path/to/directory"
-    exit 1
+  echo "使い方: $0 <MMDD> [Directory to organize]"
+  echo "例: $0 0622 /path/to/directory"
+  exit 1
 }
 
 # コマンドラインパラメータが渡されなかった場合
 if [ "$#" -ne 1 ] || [ "$#" -gt 2 ]; then
-    usage
+  usage
 fi
 
 # 入力された日付 (MMDD)
@@ -17,8 +17,8 @@ DATE=$1
 
 # 日付形式チェック (MMDD)
 if ! [[ "$DATE" =~ ^[0-1][0-9][0-3][0-9]$ ]]; then
-    echo "エラー: 無効な日付形式です。MMDD形式で入力してください。"
-    usage
+  echo "エラー: 無効な日付形式です。MMDD形式で入力してください。"
+  usage
 fi
 
 # 整理するディレクトリ(デフォルト:現在のディレクトリ)
@@ -26,8 +26,8 @@ TARGET_DIR=${2:-.}
 
 # ディレクトリの有効性チェック
 if [ ! -d "$TARGET_DIR" ]; then
-    echo "エラー: 無効なディレクトリです。"
-    usage
+  echo "エラー: 無効なディレクトリです。"
+  usage
 fi
 
 # 現在の年度と入力された日付を組み合わせてYYYYMMDD形式の日付を作成
@@ -36,8 +36,8 @@ TARGET_DATE="${CURRENT_YEAR}${DATE}"
 
 # 日付有効性チェック (例: 20240622)
 if ! date -d "$TARGET_DATE" &>/dev/null; then
-    echo "エラー: 無効な日付です。"
-    usage
+  echo "エラー: 無効な日付です。"
+  usage
 fi
 
 # "使用済みファイル"ディレクトリの作成
